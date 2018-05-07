2c64c9a844be5d48148d09de3536d23d8187d56e
Long Tom <longnv17@VISC.COM>
2018-05-07 14:35:24 +0700

    rename

--- install/debian/setup.sh
+++ install/debian/setup.sh
@@ -42,7 +42,7 @@ function install_docker_compose() {
 
 function setting_docker_proxy() {
     if [ -n $PROXY ]; then
-        mkdir /etc/systemd/system/docker.service.d
+        mkdir -p /etc/systemd/system/docker.service.d
         echo "[Service]" > /etc/systemd/system/docker.service.d/http-proxy.conf
         echo 'Environment="HTTP_PROXY="'$PROXY'" "HTTPS_PROXY='$PROXY'" "NO_PROXY=localhost,127.0.0.0/8,192.168.0.0/16"'
 
@@ -52,7 +52,7 @@ function setting_docker_proxy() {
 }
 
 function usage(){
-    echo "Install"
+    echo "use: debain --proxy proxy install docker"
 }
 
 echo "Start"

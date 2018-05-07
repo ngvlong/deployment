d16c41dd651c0f83776a71a48b5f8aae2b25bf82
Long Tom <longnv17@VISC.COM>
2018-05-07 16:40:49 +0700

    elk docker-compose

--- install/debian/docker.sh
+++ install/debian/docker.sh
@@ -1,1 +1,14 @@
-docker pull logstash:5.6.9
+#!/bin/
+
+funtion pull() {
+    docker pull logstash:5.6.9
+    docker pull elasticsearch:5.6.9
+    docker pull kibana:5.6.9
+    docker pull influxdb:1.5.2
+    docker pull shakr/statsd-influxdb
+    docker pull grafana/grafana
+}
+
+funtion install_elk(){
+    
+}
